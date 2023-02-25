0f7748ba0f0ddc87e3d733a6fc9cfd39f4050134
Blueberry <snw.wht.lk@gmail.com>
2023-02-25 22:04:39 +0100

    task1

--- src/com/epam/training/student_liudmyla_kosianova/dataType/ElectronicWatch.java
+++ src/com/epam/training/student_liudmyla_kosianova/dataType/ElectronicWatch.java
@@ -1,4 +1,5 @@
 package com.epam.training.student_liudmyla_kosianova.dataType;
+import java.util.Scanner;
 
 /**
  * The program must print an electronic watch screen output for a given value of seconds
@@ -9,6 +10,15 @@ package com.epam.training.student_liudmyla_kosianova.dataType;
  */
 
 public class ElectronicWatch {
+    protected int hours;
+    protected int minutes;
+    protected int seconds;
+    public ElectronicWatch(){
+        this.hours =0;
+        this.minutes=0;
+        this.seconds=0;
+    }
+    public void convertFromSeconds(int inputSeconds){
 
-
+    }
 }
