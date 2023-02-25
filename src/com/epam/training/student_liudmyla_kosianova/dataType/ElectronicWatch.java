0699fd94d60dcb6c27c7be84f87fd879833d5c12
Blueberry <snw.wht.lk@gmail.com>
2023-02-25 18:47:19 +0100

    task1

---
+++ src/com/epam/training/student_liudmyla_kosianova/dataType/ElectronicWatch.java
@@ -0,0 +1,14 @@
+package com.epam.training.student_liudmyla_kosianova.dataType;
+
+/**
+ * The program must print an electronic watch screen output for a given value of seconds
+ * since midnight.
+ *  Input value is given via System.in. Output value must be printed to System.out
+ *  It is guaranteed, that input number is non-negative.
+ *  Output format is h:mm:ss (possible values: [0:00:00; 23:59:59]).
+ */
+
+public class ElectronicWatch {
+
+
+}
