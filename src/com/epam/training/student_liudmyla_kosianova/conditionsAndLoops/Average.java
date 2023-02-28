efe8f96638c688d452138e310f1f5dcb69f5ebd7
Blueberry <snw.wht.lk@gmail.com>
2023-02-28 21:01:35 +0100

    Average

---
+++ src/com/epam/training/student_liudmyla_kosianova/conditionsAndLoops/Average.java
@@ -0,0 +1,21 @@
+package com.epam.training.student_liudmyla_kosianova.conditionsAndLoops;
+import java.util.Scanner;
+
+/**
+ * Write a program that reads a sequence of integer values from standard input and finds the average value.
+ *
+ * Details: *
+ * You must read sequence values until the next one is 0. Zero value means end of the input sequence.
+ * The sequence is guaranteed to contain at least one value.
+ * Average value is also an integer. Use integer operations.
+ */
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Use Scanner methods to read input
+
+    }
+
+
+}
