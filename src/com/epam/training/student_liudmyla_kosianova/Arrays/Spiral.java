e7d1e71a15e44c3d64b8166dde5ce104ca2a8773
Blueberry <snw.wht.lk@gmail.com>
2023-03-05 21:55:31 +0100

    Spiral

---
+++ src/com/epam/training/student_liudmyla_kosianova/Arrays/Spiral.java
@@ -0,0 +1,22 @@
+package com.epam.training.student_liudmyla_kosianova.Arrays;
+
+/**
+ * Please, proceed to Spiral class and implement its static method:
+ *
+ * int[][] spiral(int rows, int columns)
+ * Return a two-dimensional array coming in the form of a table and containing numbers from 1 up to rows * columns. The size of the table will be specified by the given parameters.
+ * Numbers fill the "table" clockwise from the top-level corner in a spiral manner.
+ * For example, for parameter values (3, 4), the output array should be:
+ *  1  2  3  4
+ * 10 11 12  5
+ *  9  8  7  6
+ */
+
+public class Spiral {
+    public static int[][] spiral(int rows, int columns){
+        int [][] ravlyk = new int[rows][columns];
+
+        return ravlyk;
+    }
+
+}
